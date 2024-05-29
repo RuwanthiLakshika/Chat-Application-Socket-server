5545e8a9d88b53310ab25019e7c93c781aa01e67
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-05-29 10:46:20 +0530

    make roooms to join and connection limit to room only

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,18 @@ const socket = io.connect("http://localhost:3001",{
 });
 
 function App() {
+  const [room, setRoom] = useState('');
   const [message, setMessage] = useState('');
   const [messageReceived, setMessageReceived] = useState('');
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
   
   const sendMessage = () => {
-    socket.emit('send_message',{message});
+    socket.emit('send_message',{message, room});
   };
 
   useEffect(() => {
@@ -22,6 +29,13 @@ function App() {
 
   return (
     <div className="App">
+       <input
+        placeholder="Room Number..."
+        onChange={(e) => {
+          setRoom(e.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
       <input 
       placeholder='Message...'
       onChange={(e) => setMessage(e.target.value)}
